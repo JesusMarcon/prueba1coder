b6a050c0b799cf59b83c9fad00844c932bcfc985
JesusAM <jesus@gmail>
2024-01-16 12:07:03 -0600

    creado archivo1.py y archivo2.py

---
+++ Archivo1.py
@@ -0,0 +1,6 @@
+import datetime as dt
+
+ahora = dt.datetime.now()
+
+print(f"estamos {ahora.day} del mes {ahora.month} del {ahora.year}.....")
+print(f"Hora: {ahora.hour} --- Minutos: {ahora.minute} --- Segundos: {ahora.second}")
---
+++ Archivo2.py
@@ -0,0 +1,6 @@
+import random as r
+
+#Creamos el lanzamineto de un dado
+numero_ale = r.randint(1,6)
+
+print(f"Lanzamos el dado: {numero_ale}")
