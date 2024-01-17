d76b90f5bf0fb7963f3ddc0162b9ed7c45a10db0
JesusAM <jesus@gmail>
2024-01-16 18:45:37 -0600

    Aprendinedo a gurdar

--- Archivo2.py
+++ Archivo2.py
@@ -6,4 +6,5 @@ numero_ale = r.randint(1,6)
 for x in range(1,11):
     print(f"El lanzaminetos {x}:")
     print(f"Lanzamos el dado: {numero_ale}")
-# si ponemos "git add . es como poner git add archivo 1,2,3 y etc"
+# si ponemos "git add ."" es como poner git add archivo 1,2,3 y etc"
+# si ponemos "git log" pondra los commit echos y si ponemos "git checkout" regreara a la version anterior
